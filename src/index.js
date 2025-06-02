27f2e1c445b0506caa8c5c2c8612b1683ad506de
Júlio Roberto Gomes <jackbheart@gmail.com>
2025-06-02 08:16:29 -0300

    Implementando prerrenderizacao com react-snap para SEO

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // ESTA LINHA É ESSENCIAL E DEVE ESTAR AQUI!
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) { // Se já existe HTML (vindo do pré-render)
+  ReactDOM.hydrateRoot(
+    rootElement,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else { // Se não há HTML, renderize do zero
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 reportWebVitals();
